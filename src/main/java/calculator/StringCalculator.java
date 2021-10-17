edc085bd590825c847aa697f798c86d90868b66b
Pratik Satpute <satputepratik7@gmail.com>
2021-10-17 14:58:12 +0530

    Added Methods to perform addition of numbers by removing delimiters

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,19 +3,43 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-       if(input.isEmpty()){ 
+        if (input.equals("")) {
+            return 0;
+        } else
 
+        {
+            String numList[] = splitNumbers(input, ",|\n");
+            String delimiter = ",";
+            if (input.matches("//(.*)\n(.*)")) {
+                delimiter = Character.toString(input.charAt(2));
+                input = input.substring(4);
+            }
 
-        
-        return 0;
-       }
-       else if(input.contains("1,2")){
-           return 3;
-       }
-       else{
-           System.out.println("Enter Valid String");
-           return 0;
-       }
+            String numList1[] = splitNumbers(input, delimiter + "|\n");
+            return sum(numList1);
+        }
     }
 
+    private static String[] splitNumbers(String numbers, String divider) {
+        return numbers.split(divider);
+    }
+
+    private static int sum(String[] numbers) {
+        int total = 0;
+        String negString = "";
+        for (String number : numbers) {
+            if (Integer.parseInt(number) < 0) {
+                if (negString.equals(""))
+                    negString = number;
+                else
+                    negString += ("," + number);
+            }
+            if (Integer.parseInt(number) < 1000)
+                total += Integer.parseInt(number);
+        }
+        if (!negString.equals("")) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negString);
+        }
+        return total;
+    }
 }
