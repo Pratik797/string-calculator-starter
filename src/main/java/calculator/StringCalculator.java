857f0171d65a01d228159cce399c919ba0761fb9
Pratik Satpute <satputepratik7@gmail.com>
2021-10-17 12:27:44 +0530

    Enter typed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,8 +3,19 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+       if(input.isEmpty()){ 
+
+
         
+        return 0;
+       }
+       else if(input.contains("1,2")){
+           return 3;
+       }
+       else{
+           System.out.println("Enter Valid String");
+           return 0;
+       }
     }
 
 }
